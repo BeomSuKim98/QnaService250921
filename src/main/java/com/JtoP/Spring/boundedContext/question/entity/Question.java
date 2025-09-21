a3f9a7ef8dc3b3ff2928988478ad9307eb08dc74
BeomSuKim98 <qja0427@gmail.com>
2025-09-22 00:25:43 +0900

    양방향 관계수립 처리

--- src/main/java/com/JtoP/Spring/boundedContext/question/entity/Question.java
+++ src/main/java/com/JtoP/Spring/boundedContext/question/entity/Question.java
@@ -31,5 +31,11 @@ public class Question {
     // CasecadeType.REMOVE 질문이 삭제되면 그 안에 달려있는 답변도 같이 삭제 됌
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    @Builder.Default // 빌더 패턴으로 객체를 생성할 때 필드 초기화
+    private List<Answer> answerList = new ArrayList<>();
+
+    public void addAnswer(Answer answer){
+        answer.setQuestion(this); // Question 객체에 Answer 객체를 추가
+        answerList.add(answer); // answerList에 답변 데이터 추가 이 작업을 통해 양방향 연관관계 설정
+    }
 }
