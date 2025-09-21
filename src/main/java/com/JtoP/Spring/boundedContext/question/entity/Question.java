3f4081f8403b7f6b7b25aaff9e6ef83346525662
BeomSuKim98 <qja0427@gmail.com>
2025-09-22 00:26:53 +0900

    양방향 관계수립 처리 주석 설명

--- src/main/java/com/JtoP/Spring/boundedContext/question/entity/Question.java
+++ src/main/java/com/JtoP/Spring/boundedContext/question/entity/Question.java
@@ -37,5 +37,18 @@ public class Question {
     public void addAnswer(Answer answer){
         answer.setQuestion(this); // Question 객체에 Answer 객체를 추가
         answerList.add(answer); // answerList에 답변 데이터 추가 이 작업을 통해 양방향 연관관계 설정
+
+        /*
+        초기화 하지 않게 되면 NullPointerException이 발생할 수 있다.
+        ex) q2.getAnswerList().add(answer);
+        두번 째 질문에서 리스트를 호출했지만 해당 리스트가 초기화 되어 있지 않으면 null한테 add() 를 호출함으로 인해 NullPointerException이 발생
+        JPA는 클래스 내부에 있는 필드 값을 초기화 시키지 않는다.
+        new ArrayList(); -> 이렇게 초기화를 해두면 컬렉션 조작이 안전하다.
+
+        질문(Question) 입장에서 해당 질문에 달려있는 답변 목록(answerList)을 알고 있어야 한다.
+        답변(Answer) 입장에서 자신이 속한 질문(Question)을 알고 있어야 한다.
+        질문은 자신에게 달린 답변이 무엇인지 알고 있어야 한다.
+        답변은 자신이 어떤 질문에 속해 있는지 알고 있어야 한다.
+         */
     }
 }
