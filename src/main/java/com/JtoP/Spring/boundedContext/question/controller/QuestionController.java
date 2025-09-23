c1cdddf1911688cba2783be17b6c1e5d2fbc450c
BeomSuKim98 <qja0427@gmail.com>
2025-09-23 20:07:12 +0900

    model 객체를 이용해서 템플릿에 데이터 전달

--- src/main/java/com/JtoP/Spring/boundedContext/question/controller/QuestionController.java
+++ src/main/java/com/JtoP/Spring/boundedContext/question/controller/QuestionController.java
@@ -1,17 +1,30 @@
 package com.JtoP.Spring.boundedContext.question.controller;
 
+import com.JtoP.Spring.boundedContext.question.entity.Question;
+import com.JtoP.Spring.boundedContext.question.repository.QuestionRepository;
 import org.springframework.ui.Model;
+
+import lombok.RequiredArgsConstructor;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/question")
+@RequiredArgsConstructor
 public class QuestionController {
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/list")
     public String showList(Model model){
-        model.addAttribute("age,20");
+        // findALL() : 모든 데이터를 조회
+        // SELECT * FROM question
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
         return "question/question_list";
     }
 }
